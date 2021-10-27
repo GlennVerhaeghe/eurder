83d8c2a2415f67e833763a2eb5bc5c077468dc39
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-10-27 11:11:22 +0200

    init

---
+++ src/test/java/be/glenn/eurder/EurderApplicationTests.java
@@ -0,0 +1,13 @@
+package be.glenn.eurder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
